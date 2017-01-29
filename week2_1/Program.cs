5767aa6e2a0fba336ff703941ac77d3ed62562b0
Altynaay <yerkhassym.altynay@gmail.com>
2017-01-29 21:56:31 +0600

    week2_1second

--- week2_1/Program.cs
+++ week2_1/Program.cs
@@ -1,38 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApplication2
-{
-    class Program
-    {
-        static void Rcr(string path, int level)
-            //вывести папки и файлы до второго уровня 
-        {  
-            if (level > 2)
-                return;
-                DirectoryInfo directory = new DirectoryInfo(path);
-                FileInfo[] files = directory.GetFiles();
-                DirectoryInfo[] directories = directory.GetDirectories();
-                //вывести названия всех файлов 
-                foreach (FileInfo file in files)
-                {
-                    emptySpace(level);
-                    Console.WriteLine(file.Name);
-                }
-                // вывести названия всех папок с пробелом 
-                foreach (DirectoryInfo dInfo in directories)
-                {
-                    emptySpace(level);
-                    Console.WriteLine(dInfo.Name);
-                    Rcr(dInfo.FullName, level + 1);
-                }
+﻿ using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Text;
+ using System.Threading.Tasks;
+ using System.IO;
+ namespace Student
+ {
+     class MainClass
+     {
+         static void Pr1()
+         { // считывает данные с указанного файла 
+             StreamReader sr = new StreamReader(@"C:\Users\Admin\Documents\input.txt");
+             string[] arr = sr.ReadLine().Split(); //записывает данные с файла в массив в виде стрингов 
+             int min = 99999, max = -99999;
+             foreach (string s in arr)
+            {
+                 int p = int.Parse(s); //переводит стринги в инты 
+                 if (p<min)
+                     min = p;
+                 if (p > max)
+                     max = p;
+             }
+             Console.WriteLine(min + " " + max); //выводит на экран маскимальное и минимальное числа через пробел 
+             Console.ReadKey();
+             sr.Close();
          }
-        static void Main(string[] args)
-        {
-            Rcr(@"C:\Users\Admin", 0);
-        }
+         static void Main(string[] args)
+         {
+             Pr1();
+         }
     }
-}
+ }
