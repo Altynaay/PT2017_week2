c556088997bd598be220128db6386a6547e4a505
Altynaay <yerkhassym.altynay@gmail.com>
2017-01-29 22:00:57 +0600

    week2_2second

--- week2_1/Program.cs
+++ week2_1/Program.cs
@@ -1,33 +1,43 @@
-﻿ using System;
- using System.Collections.Generic;
- using System.Linq;
- using System.Text;
- using System.Threading.Tasks;
- using System.IO;
- namespace Student
- {
-     class MainClass
-     {
-         static void Pr1()
-         { // считывает данные с указанного файла 
-             StreamReader sr = new StreamReader(@"C:\Users\Admin\Documents\input.txt");
-             string[] arr = sr.ReadLine().Split(); //записывает данные с файла в массив в виде стрингов 
-             int min = 99999, max = -99999;
-             foreach (string s in arr)
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Student
+{
+    class MainClass
+    {
+        static bool isPrime(int a) // проверяет простое ли число
+        {
+            if (a == 1)
+                return false;
+            for (int i = 2; i * i < a; i++)
+                if (a % i == 0)
+                    return false;
+            return true;
+        }
+        static void Pr1()
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Admin\Documents\input.txt"); // считывает данные с указанног файла 
+            StreamWriter sw = new StreamWriter(@"C:\Users\Admin\Documents\output.txt"); // записывает полученный результат в файл 
+            string[] arr = sr.ReadLine().Split(); // записывает данные в массив стрингов 
+            int x = 99999;
+            foreach (string s in arr)
             {
-                 int p = int.Parse(s); //переводит стринги в инты 
-                 if (p<min)
-                     min = p;
-                 if (p > max)
-                     max = p;
-             }
-             Console.WriteLine(min + " " + max); //выводит на экран маскимальное и минимальное числа через пробел 
-             Console.ReadKey();
-             sr.Close();
-         }
-         static void Main(string[] args)
-         {
-             Pr1();
-         }
+                int p = int.Parse(s); //  переводит в инты стринги 
+                if (isPrime(p))
+                    if (p < x)
+                        x = p;
+            }
+            sw.WriteLine(x); 
+            sw.Close();
+            sr.Close();
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            Pr1();
+        }
     }
- }
+}
