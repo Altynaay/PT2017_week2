41fc81fb6ea408d66737e48b8d39b4550cda5127
Altynaay <yerkhassym.altynay@gmail.com>
2017-01-29 21:41:35 +0600

    week21

--- week2_1/Program.cs
+++ week2_1/Program.cs
@@ -9,23 +9,25 @@ namespace ConsoleApplication2
     class Program
     {
         static void Rcr(string path, int level)
-        {
+            //вывести папки и файлы до второго уровня 
+        {  
             if (level > 2)
                 return;
                 DirectoryInfo directory = new DirectoryInfo(path);
                 FileInfo[] files = directory.GetFiles();
                 DirectoryInfo[] directories = directory.GetDirectories();
-
+                //вывести названия всех файлов 
                 foreach (FileInfo file in files)
                 {
                     emptySpace(level);
                     Console.WriteLine(file.Name);
                 }
+                // вывести названия всех папок с пробелом 
                 foreach (DirectoryInfo dInfo in directories)
                 {
                     emptySpace(level);
                     Console.WriteLine(dInfo.Name);
-                    Ex4(dInfo.FullName, level + 1);
+                    Rcr(dInfo.FullName, level + 1);
                 }
          }
         static void Main(string[] args)
